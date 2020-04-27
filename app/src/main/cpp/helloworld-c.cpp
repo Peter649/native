6dfb41d7df9bab3f8bf476a690509acaead33fca
Peter <zeratul3636@hotmail.com>
2020-04-27 16:10:34 +0900

    second

---
+++ app/src/main/cpp/helloworld-c.cpp
@@ -0,0 +1,21 @@
+#include <jni.h>
+#include <native_app_glue/android_native_app_glue.h>
+
+extern "C" {
+void handle_cmd(android_app *pApp, int32_t cmd) {
+}
+
+void android_main(struct android_app *pApp) {
+    pApp->onAppCmd = handle_cmd;
+
+    int events;
+    android_poll_source *pSource;
+    do {
+        if (ALooper_pollAll(0, nullptr, &events, (void **) &pSource) >= 0) {
+            if (pSource) {
+                pSource->process(pApp, pSource);
+            }
+        }
+    } while (!pApp->destroyRequested);
+}
+}
